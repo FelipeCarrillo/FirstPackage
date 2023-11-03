e0f3ea4e00af2c0a31ef92dee0a729dac244b65e
Felipe <mazatlanmexico@yahoo.com>
2023-11-02 22:14:25 -0700

    Update function to include 'remove ='

--- R/one2many2.R
+++ R/one2many2.R
@@ -11,13 +11,12 @@
 #' p_load(tidyverse, Firstpackage)
 #' a <- data.frame(name= c("Maria", "Tina"), age= c(5,3))
 #' a
-#' one2many2(a,'age')
+#' one2many2(a, age)
 
 library(tidyr)
 library(tidyverse)
 library(dplyr)
-one2many2 <- function(data, var){
-data <- data %>% mutate(rows = row_number()) |>
-uncount(data[,var])
-return(data)
+one2many2 <- function(data, var,...){
+data %>% mutate(rows = row_number()) |>
+uncount({{var}},...)
 }
